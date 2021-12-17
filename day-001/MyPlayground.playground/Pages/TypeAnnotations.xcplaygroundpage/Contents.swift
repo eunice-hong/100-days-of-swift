45b57d2cdb3e42260eb73e094746b6dae8102919
eunice-hong <dev@eunice-hong.kr>
2021-12-18 00:43:18 +0900

    feat: create type annotations

---
+++ day-001/MyPlayground.playground/Pages/TypeAnnotations.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+//: [Previous](@previous)
+
+import Foundation
+
+let greeting = "Hello, playground"
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
+
+//: [Next](@next)

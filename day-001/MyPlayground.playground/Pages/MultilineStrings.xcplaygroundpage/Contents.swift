0bdb5a92aa442a8ca226cbb79cfc0330fe736c38
eunice-hong <dev@eunice-hong.kr>
2021-12-18 00:36:53 +0900

    feat: create multiline strings

---
+++ day-001/MyPlayground.playground/Pages/MultilineStrings.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var greeting = """
+This goes
+over multiple
+lines
+"""
+
+
+var str2 =  """
+This goes \
+over multiple \
+lines
+"""
+
+//: [Next](@next)
